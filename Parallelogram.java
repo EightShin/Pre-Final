61d1cd2aa4f95286e41fac3811f82f2458bad23a
EightShin <trickxter15@gmail.com>
2025-10-11 17:02:02 +0800

    Cleaned

---
+++ Parallelogram.java
@@ -0,0 +1,6 @@
+public class Parallelogram extends Quadrilateral {
+    public void showDescription() {
+        super.showDescription();
+        System.out.println("- has 2 pairs of parallel sides");
+    }
+}
